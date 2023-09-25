113e5760708741f1c2b109819f9c9084f725299f
teodorstupnicki <teodor.stupnicki@gmail.com>
2023-09-25 22:51:45 +0200

    Added lsp config file

---
+++ nvchad/configs/lspconfig.lua
@@ -0,0 +1,40 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.rust_analyzer.setup({
+   on_attach = on_attach,
+   capabilities = capabilities,
+   filetypes = {"rust"},
+   root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+   settings = {
+     ['rust-analyzer'] = {
+       cargo = {
+          allFeatures = true,
+        },
+      },
+    },
+ })
+
+ lspconfig.tsserver.setup {
+   on_attach = on_attach,
+   capabilities = capabilities,
+   init_options = {
+     preferences = {
+       disableSuggestions = true,
+     }
+   }
+ }
+-- 
+-- lspconfig.pyright.setup { blabla}
