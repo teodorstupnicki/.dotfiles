18f8a44d272da56b050493e3caaf149b02f35924
Teodor Stupnicki <teodor.stupnicki@gmail.com>
2024-12-30 18:58:49 +0100

    Added setup script for Mac

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# Get sudo
+#sudo -v
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Enable brew
+(
+  echo
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+) >>/Users/mrf/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install packages and casks with brew
+echo "Installing programs with homebrew"
+brew update
+brew upgrade
+
+brew install --cask 1password 1password-cli arc discord karabiner-elements orbstack raycast rectangle-pro shottr visual-studio-code
+
+brew install corepack deno dockutil fnm gh git httpie iperf3 node plow stripe tfenv tmux fzf
