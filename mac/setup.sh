9913ef6bd9d276cdc7f8d0e2c096dda6c03dbb2d
Teodor Stupnicki <teodor.stupnicki@gmail.com>
2025-01-09 19:30:11 +0100

    Added foundry installation in setup.sh

--- mac/setup.sh
+++ mac/setup.sh
@@ -19,3 +19,7 @@ brew upgrade
 brew install --cask 1password 1password-cli arc discord karabiner-elements orbstack raycast rectangle-pro shottr visual-studio-code
 
 brew install corepack deno dockutil fnm gh git httpie iperf3 node plow stripe tfenv tmux fzf
+
+# Install and setup foundry
+/bin/bash -c "$(curl -L https://foundry.paradigm.xyz)" | bash
+foundryup
